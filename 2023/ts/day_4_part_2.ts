863e57b4d2465591fd5646f531659706a8945d7c
njberman <njberman16@hotmail.com>
2024-07-25 22:15:36 +0100

    FINALLY COMPLETE DAY 4 PART 2

--- 2023/ts/day_4_part_2.ts
+++ 2023/ts/day_4_part_2.ts
@@ -5,6 +5,7 @@ interface Card {
   index: number;
   winningNumbers: number[];
   yourNumbers: number[];
+  matching: number;
 }
 
 interface Node {
@@ -13,10 +14,23 @@ interface Node {
   right?: Node;
 }
 
-const file = fs.readFileSync("./test.txt");
+function countMatching(
+  winningNumbers: number[],
+  yourNumbers: number[],
+): number {
+  let count = 0;
+  for (const n of yourNumbers) {
+    if (winningNumbers.includes(n)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const file = fs.readFileSync("./day_4_input.txt");
 const lines = String(file).split("\n").slice(0, -1);
 
-const cards: Card[] = [];
+const globalCards: Card[] = [];
 
 for (const line of lines) {
   const [winningNumbers, yourNumbers] = line
@@ -29,77 +43,26 @@ for (const line of lines) {
         .map((n) => parseInt(n)),
     );
 
-  cards.push({ winningNumbers, yourNumbers, index: lines.indexOf(line) });
+  globalCards.push({
+    winningNumbers,
+    yourNumbers,
+    index: lines.indexOf(line),
+    matching: countMatching(winningNumbers, yourNumbers),
+  });
 }
 
-const processed: number[] = [];
-function process(card: Card, cards: Card[], count: number = 0) {
-  const matching = countMatching(card);
-  if (matching === 0 || cards.indexOf(card) === cards.length - 1) return 1;
+const allCards = Array.from({ length: globalCards.length }, () => 1);
 
-  for (let i = 0; i < matching; i++) {
-    if (cards.indexOf(card) + i + 1 >= cards.length) break;
-    processed.push(cards.indexOf(card) + i + 2);
-    count += process(cards[cards.indexOf(card) + i + 1], cards, count);
+for (let index = 0; index < globalCards.length; index++) {
+  const card = globalCards[index];
+  const { matching } = card;
+
+  for (let i = 0; i < allCards[index]; i++) {
+    const finalIndex = index + matching + 1;
+    for (let newIndex = index + 1; newIndex < finalIndex; newIndex++) {
+      allCards[newIndex]++;
+    }
   }
-
-  return count;
 }
 
-console.log(process(cards[0], cards), processed);
-// function generateTree(node: Node, cards: Card[], matchingLeft: number): Node {
-//   if (node.val === cards.length || matchingLeft === 0) return node;
-//   const card = cards[node.val - 1];
-//
-//   const matching = countMatching(card);
-//   if (matching === 0) return node;
-//
-//   if (node.left) node.right = generateTree(node.left, cards, matching - 1);
-//   else node.left = generateTree(node, cards, matching - 1);
-//   return node;
-// }
-
-// function generateTree(card: Card, cards: Card[], lastMatching: number): Node {
-//   const root: Node = { val: cards.indexOf(card) + 1 };
-//   if (lastMatching === 0) return root;
-//
-//   root.right = generateTree(
-//     cards[cards.indexOf(card) + 1],
-//     cards,
-//     lastMatching - 1,
-//   );
-//
-//   const matching = countMatching(card);
-//   if (matching === 0 || cards.indexOf(card) === cards.length - 1) return root;
-//
-//   root.left = generateTree(cards[cards.indexOf(card) + 1], cards, matching - 1);
-//   return root;
-// }
-
-function countMatching(card: Card): number {
-  return card.yourNumbers.reduce(
-    (acc, el) => (card.winningNumbers.includes(el) ? acc + 1 : acc),
-    0,
-  );
-}
-
-// console.log(
-//   util.inspect(
-//     generateTree(
-//       {
-//         val: 1,
-//         left: {
-//           val: 2,
-//           left: {
-//             val: 3,
-//             left: { val: 4, left: { val: 5, left: { val: 6 } } },
-//           },
-//         },
-//       },
-//       cards,
-//     ),
-//     false,
-//     null,
-//     true,
-//   ),
-// );
+console.log(allCards.reduce((acc, el) => acc + el));
