9d0471acd71b3afb77dd863e6c1ec3d9f9fec98d
Netanel Berman <njberman16@hotmail.com>
2024-07-26 11:06:45 +0100

    was using maps; worked fine for the example, but as soon as I ran it on the actual input, it said I'd used up the max map size, so I went straight to using just regular JS objects, and then RAN OUT OF MEMORY! need to find a memory efficient way of doing this; possibly storing the values on disk. Will be slower but might actually work

--- 2023/ts/day_5_part_1.ts
+++ 2023/ts/day_5_part_1.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-let lines = fs.readFileSync("./test-input.txt").toString().split("\n\n");
+let lines = fs.readFileSync("./day_5_input.txt").toString().split("\n\n");
 
 const seedsIds = lines[0]
   .slice(7)
@@ -9,13 +9,40 @@ const seedsIds = lines[0]
 
 const givenMaps = lines.splice(1).filter((s) => s !== "");
 
-// const map: { [mapName: string]:  } = [];
+const maps: { [mapName: string]: { [id: number]: number } } = {};
 
 for (const givenMap of givenMaps) {
-  const [sourceCategory, destinationCategory] = givenMap
-    .split("\n")[0]
-    .split(" ")[0]
-    .split("-to-");
+  const splitGivenMap = givenMap.split("\n").filter((s) => s !== "");
+  const mapName = splitGivenMap[0].split(" ")[0].split("-to-").join("-");
 
-  // const destinationRangeStart
+  const currentMap: { [id: number]: number } = {};
+
+  for (const map of splitGivenMap.slice(1)) {
+    const [destinationRangeStart, sourceRangeStart, rangeLength] = map
+      .split(" ")
+      .map((s) => parseInt(s));
+
+    for (let i = 0; i < rangeLength; i++) {
+      currentMap[sourceRangeStart + i] = destinationRangeStart + i;
+    }
+  }
+
+  maps[mapName] = currentMap;
 }
+
+let nextIds = seedsIds.slice();
+for (const mapKey in maps) {
+  const currentMap = maps[mapKey];
+  const newIds: number[] = [];
+  for (const id of nextIds) {
+    if (currentMap) {
+      let newId = currentMap[id];
+      if (!newId) newId = id;
+      newIds.push(newId);
+    }
+  }
+  nextIds = newIds.slice();
+}
+
+const lowestLocationId = Math.min(...nextIds);
+console.log(lowestLocationId);
