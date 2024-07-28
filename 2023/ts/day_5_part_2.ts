34b617e09a4f9d355d7a38e1072bc3bb150eec28
njberman <njberman16@hotmail.com>
2024-07-28 13:51:04 +0100

    finished day 5 part 2; thanks to Krimsonfreak on r/adventofcode otherwise i wouldn't have come up with a solution

--- 2023/ts/day_5_part_2.ts
+++ 2023/ts/day_5_part_2.ts
@@ -1,5 +1,7 @@
 import fs from "fs";
 
+const startTime = performance.now();
+
 let lines = fs.readFileSync("./day_5_input.txt").toString().split("\n\n");
 
 const givenMaps = lines
@@ -28,9 +30,31 @@ for (const givenMap of givenMaps) {
   }
 }
 
-function getLocation(seedId: number): number {
+function getSeedId(locationId: number): number {
+  let nextIds = [locationId];
+  for (const mapName of Object.keys(maps).reverse()) {
+    const currentMap = maps[mapName];
+    const newIds: number[] = [];
+    for (const id of nextIds) {
+      let newId = id;
+      for (const map of currentMap) {
+        const { source, destination, range } = map;
+
+        if (destination <= id && id < destination + range) {
+          newId = id - destination + source;
+          break;
+        }
+      }
+      newIds.push(newId);
+    }
+    nextIds = newIds.slice();
+  }
+  return nextIds[0];
+}
+
+function getLocationId(seedId: number): number {
   let nextIds = [seedId];
-  for (const mapName in maps) {
+  for (const mapName of Object.keys(maps)) {
     const currentMap = maps[mapName];
     const newIds: number[] = [];
     for (const id of nextIds) {
@@ -50,21 +74,36 @@ function getLocation(seedId: number): number {
   return nextIds[0];
 }
 
+// console.log(0, getLocationId(0), getSeedId(getLocationId(0)));
+// console.log(maps);
+
 const seedsStartsAndRanges = lines[0]
   .slice(7)
   .split(" ")
   .map((s) => parseInt(s));
 
-const locations: number[] = [];
-for (let index = 0; index < seedsStartsAndRanges.length; index++) {
-  console.log(index);
-  if (index % 2 === 0) locations.push(getLocation(seedsStartsAndRanges[index]));
-  else {
-    for (let i = 1; i < seedsStartsAndRanges[index]; i++) {
-      locations.push(getLocation(seedsStartsAndRanges[index + i]));
-      console.log(index + i);
+let locationId = 0;
+let seedId = getSeedId(locationId);
+while (true) {
+  let shouldWeBreak = false;
+  for (let i = 0; i < seedsStartsAndRanges.length / 2; i++) {
+    const start = seedsStartsAndRanges[2 * i];
+    const range = seedsStartsAndRanges[2 * i + 1];
+
+    if (start <= seedId && seedId < start + range) {
+      shouldWeBreak = true;
+      break;
     }
   }
+  if (shouldWeBreak) break;
+  locationId++;
+  seedId = getSeedId(locationId);
+
+  if (locationId > 1000000000) break;
 }
 
-console.log(Math.min(...locations));
+console.log(locationId, seedId, getLocationId(seedId), getSeedId(locationId));
+
+const endTime = performance.now();
+
+console.log(`Program took: ${(endTime - startTime) / 1000}s.`);
