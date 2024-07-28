6ce5514ef74a7c8189b8800555a6bff38fe0ca21
njberman <njberman16@hotmail.com>
2024-07-28 18:34:53 +0100

    add rust for day 6; init day 6 part 1

---
+++ 2023/rust/day_6_part_1/target/debug/day_6_part_1.d
@@ -0,0 +1,1 @@
+/home/nato/dev/advent-of-code/2023/rust/day_6_part_1/target/debug/day_6_part_1: /home/nato/dev/advent-of-code/2023/rust/day_6_part_1/src/main.rs
---
+++ 2023/rust/day_6_part_1/target/debug/deps/day_6_part_1-a723470e71c10f89.d
@@ -0,0 +1,5 @@
+/home/nato/dev/advent-of-code/2023/rust/day_6_part_1/target/debug/deps/day_6_part_1-a723470e71c10f89: src/main.rs
+
+/home/nato/dev/advent-of-code/2023/rust/day_6_part_1/target/debug/deps/day_6_part_1-a723470e71c10f89.d: src/main.rs
+
+src/main.rs:
